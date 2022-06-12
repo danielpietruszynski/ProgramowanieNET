64b70d3425ba4b8739be2e79e542a51c699b7b2f
Daniel Pietruszyński <79590271+danielpietruszynski@users.noreply.github.com>
2022-06-12 17:00:32 +0200

    Add files via upload

---
+++ NET-2405/NET-2405/Outside.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_2405
+{
+    class Outside
+    {
+        public class Inside
+        {
+            public void algorytm(float liczba)
+            {
+                float dok = 0.0000001f;
+                
+                float x = liczba / 2;
+                
+                while(Math.Abs(x - (liczba / x)) > dok)
+                {
+                    x = (x + (liczba / x)) / 2;
+                    
+                    if (x * x == 2)
+                    {
+                        Console.WriteLine(x);
+                    }
+                }
+                
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
---
+++ NET-2405/NET-2405/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_2405
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Outside.Inside inside = new Outside.Inside();
+            inside.algorytm(3);
+            Console.ReadKey();
+        }
+    }
+}
